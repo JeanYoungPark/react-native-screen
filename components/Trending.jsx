a8b75165bc67aedfd1bc173473d41d1c3c3f7d8c
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2024-05-15 14:47:58 +0900

    fix: key 수정

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -2,7 +2,18 @@ import { View, Text, FlatList } from "react-native";
 import React from "react";
 
 const Trending = ({ posts }) => {
-    return <FlatList data={posts} keyExtractor={(item) => item.$id} renderItem={({ item }) => <Text className='text-3xl text-white'>{item.id}</Text>} horizontal />;
+    return (
+        <FlatList
+            data={posts}
+            keyExtractor={(item) => item.$id}
+            renderItem={({ item, idx }) => (
+                <Text key={idx} className='text-3xl text-white'>
+                    {item.id}
+                </Text>
+            )}
+            horizontal
+        />
+    );
 };
 
 export default Trending;
